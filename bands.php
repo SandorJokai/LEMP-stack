451808eb7b9c11e41153a4eb63916ad81021fd94
shanxman1986@gmail.com <shanxman1986@gmail.com>
2021-06-20 21:37:09 +0100

    add more files

---
+++ bands.php
@@ -0,0 +1,44 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+try{
+    $pdo = new PDO("mysql:host=44.193.197.254;dbname=example", "example-user", "password");
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+ 
+// Attempt select query execution
+try{
+    $sql = "SELECT * FROM bands";   
+    $result = $pdo->query($sql);
+    if($result->rowCount() > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>city</th>";
+                echo "<th>band</th>";
+            echo "</tr>";
+        while($row = $result->fetch()){
+            echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['city'] . "</td>";
+                echo "<td>" . $row['band'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        unset($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} catch(PDOException $e){
+    die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+}
+ 
+// Close connection
+unset($pdo);
+
+?>
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>This is a virtualHost :)</title>
+</head>
+<body>
+	<h1>Welcome to my virtualhost running on nginx!</h1>
+</body>
+
+</html>
